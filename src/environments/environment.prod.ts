5c8552693634a0c9a2672ddd399cf57f9242ccc0
sarank17 <ksaran17vpk@outlook.com>
2026-05-04 00:18:25 +0530

    environment file update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://tiny-url-services-a0gba9bgggb8dsfk.centralus-01.azurewebsites.net/api'
+};
--- src/url-management/url-management.service.ts
+++ src/url-management/url-management.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment'
 
 @Injectable({ providedIn: 'root' })
 export class UrlService {
-  private apiUrl = 'https://localhost:7072/api/Url'; 
+  //private apiUrl = 'https://localhost:7072/api/Url';
+  private apiUrl = environment.apiUrl; 
 
   constructor(private http: HttpClient) { }
 
